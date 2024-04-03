05b7254949964cba3c82754e0befa90ffe1d3769
John Paul Quintana <jpquintana2024@gmail.com>
2024-04-03 17:28:42 +0800

    Update sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -168,7 +168,7 @@
                         {{-- @endif --}}
 
                         <!-- Menu Item Dashboard -->
-                        <li>
+                        {{-- <li>
                             <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
                                 href="{{ route('evaluated') }}"
                                 :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Dashboard') }">
@@ -194,7 +194,7 @@
                             </a>
 
 
-                        </li>
+                        </li> --}}
 
                     </ul>
                 </div>
